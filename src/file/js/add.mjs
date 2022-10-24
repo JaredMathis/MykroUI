5750fd72c6fd8c88ce46340325078d96e87e9561
JaredMathis <jared.mathis@native.consulting>
2022-10-24 16:44:27 -0400

    noop refactor 2022-10-24T20:44:27.267Z

--- src/file/js/add.mjs
+++ src/file/js/add.mjs
@@ -1,10 +1,13 @@
 import path from "path";
 import { mykro_directory_source } from "../../mykro/directory/source.mjs";
-import { file_add } from "../add.mjs";
+import { file_write } from "../write.mjs";
 import { file_js_name_to_path } from "./name/to/path.mjs";
 
 export async function file_js_add(function_name) {
     const path_without_extension = path.join(mykro_directory_source(), file_js_name_to_path(function_name));
     let file_path = path_without_extension + '.mjs';
-    await file_add(file_path)
+    await file_write(file_path, `export function ${function_name}() {
+    
+}
+`)
 }
