9b9b6c3fb179e30c29976aa2267a126ffb73335d
JaredMathis <jared.mathis@native.consulting>
2022-10-24 15:10:06 -0400

    progress

--- src/m.js
+++ src/m.js
@@ -1,2 +1,22 @@
-let file_path = process.argv[0];
-console.log(file_path);
+let file_path = process.argv[2];
+if (!file_path) {
+    error('Expecting command line argument')
+}
+
+async function run() {
+    let imported = await import('./' + string_replace_all(process.argv[2], '_', '/') + '.mjs');
+    imported
+}
+
+run();
+
+function property_get(obj, name) {
+    if (!obj.hasOwnProperty(name)) {
+        error('TODO');
+    }
+    return obj[name];
+}
+
+function string_replace_all(s, from, to) {
+    return s.split(from).join(to);
+}
