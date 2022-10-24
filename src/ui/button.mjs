010c81c9ef56b86d57f7e05e1c73159fc132e15f
JaredMathis <jared.mathis@native.consulting>
2022-10-24 16:51:36 -0400

    noop progress 2022-10-24T20:51:36.027Z

--- src/ui/button.mjs
+++ src/ui/button.mjs
@@ -1,5 +1,5 @@
 export function ui_button(parent, text) {
-    let button = ui_element(parent);
+    let button = ui_element(parent, 'button');
     ui_element_text(button, text);
     return button;
 }
