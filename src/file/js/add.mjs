be46a3753416281f1ec9199cc5099adec60effdf
JaredMathis <jared.mathis@native.consulting>
2022-10-24 16:33:54 -0400

    file_add src/js/function/name/to/path.mjs 2022-10-24T20:33:54.105Z

--- src/file/js/add.mjs
+++ src/file/js/add.mjs
@@ -1,5 +1,7 @@
+import path from "path";
+import { mykro_directory_source } from "../../mykro/directory/source.mjs";
 import { file_add } from "../add.mjs";
 
 export function file_js_add(function_name) {
-    file_add()
+    file_add(path.join([mykro_directory_source(), ]))
 }
---
+++ src/mykro/directory/source.mjs
@@ -0,0 +1,3 @@
+export function mykro_directory_source() {
+    return './src'
+}
