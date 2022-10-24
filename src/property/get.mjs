16fe634e638a8f9c22c2df7de97237a6433d39d8
JaredMathis <jared.mathis@native.consulting>
2022-10-24 16:47:04 -0400

    noop refactor 2022-10-24T20:47:04.521Z

--- src/property/get.mjs
+++ src/property/get.mjs
@@ -1,3 +1,10 @@
-export function property_get() {
-    
+import { error } from "../error.mjs";
+
+export function property_get(obj, name) {
+    let keys = Object.keys(obj);
+    // console.log({keys,name});
+    if (!keys.includes(name)) {
+        error('TODO');
+    }
+    return obj[name];
 }
