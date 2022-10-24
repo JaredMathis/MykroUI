d68141f7250a022ec88c8f4752ee638590c56649
JaredMathis <jared.mathis@native.consulting>
2022-10-24 16:36:57 -0400

    noop refactor 2022-10-24T20:36:57.884Z

--- src/file/js/name/to/path.mjs
+++ src/file/js/name/to/path.mjs
@@ -1,3 +1,6 @@
+import { string_replace_all } from "../../../../string/replace/all.mjs";
+
 export function file_js_name_to_path(function_name) {
-    
+    const replaced = string_replace_all(function_name, '_', '/');
+    return replaced;
 }
