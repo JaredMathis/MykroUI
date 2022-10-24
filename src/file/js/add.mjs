cd44e4497cfaa0fb6b9b85ac90de3974a92031a7
JaredMathis <jared.mathis@native.consulting>
2022-10-24 16:41:58 -0400

    file_add src/file/write.mjs 2022-10-24T20:41:58.491Z

--- src/file/js/add.mjs
+++ src/file/js/add.mjs
@@ -3,9 +3,7 @@ import { mykro_directory_source } from "../../mykro/directory/source.mjs";
 import { file_add } from "../add.mjs";
 import { file_js_name_to_path } from "./name/to/path.mjs";
 
-export function file_js_add(function_name) {
+export async function file_js_add(function_name) {
     let file_path = path.join(mykro_directory_source(), file_js_name_to_path(function_name)) + '.mjs';
-    console.log({file_path});
-    return;
-    file_add(file_path)
+    await file_add(file_path)
 }
