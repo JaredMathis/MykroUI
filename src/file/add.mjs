7b63aa54108e967ff46921aa1fbe308970dc6537
JaredMathis <jared.mathis@native.consulting>
2022-10-24 15:33:52 -0400

    rename

--- src/file/add.mjs
+++ src/file/add.mjs
@@ -4,16 +4,16 @@ import path from 'path';
 
 export async function file_add(file_name) {
     let directory_name = path.dirname(file_name);
-    if (!await checkFileExists(directory_name)) {
+    if (!await file_exists(directory_name)) {
         await fs.promises.mkdir(directory_name, { recursive: true });
     }
-    if (await checkFileExists(file_name)) {
+    if (await file_exists(file_name)) {
         error(`${file_name} exists`);
     }
     await fs.promises.writeFile(file_name, '');
 }
 
-function checkFileExists(file) {
+function file_exists(file) {
     return fs.promises.access(file, fs.constants.F_OK)
              .then(() => true)
              .catch(() => false)
